a0d460dd41b3e7aa36d53b2a543acffe74e2b405
Ana Marija Okorn <ana-marija.okorn@student.fmf.uni-lj.si>
2021-01-27 09:12:53 +0100

    ureditev vizualizacija.r

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -12,37 +12,12 @@ povprecje.diplomantov.po.regijah <- povprecje.diplomantov.po.regijah[-12,] %>% s
 
 # GRAF POVPRECJA DIJAKOV IN DIPLOMANTOV PO REGIJAH
 
-#graf1 <- ggplot(povprecje.dijakov.po.regijah, aes(regija, povprecje, group = 1)) + geom_col() + coord_flip() +
-#  labs(x = "Regija", y = "Povprečje", 
-#       title = "Povprečno število dijakov po regijah v zadnjih desetih letih")
-
-graf_povprecja_po_regijah <- ggplot(povprecje.dijakovindiplomantov.po.regijah, aes(x=regija, y=povprecje, fill=kategorija)) + geom_col(position = 'dodge')  + 
-  coord_flip() + labs(x = "Regija", y = "Povprečno število", title = "Povprečno število dijakov in diplomantov na leto v \nposameznih statističnih regijah") +
-  scale_fill_discrete(name = "Kategorija", labels = c("število dijakov", "število diplomantov")) + scale_fill_brewer()
-
-# ZEMLJEVID POVPRECJA DIPLOMANTOV PO REGIJAH
-
-zemljevid <- uvozi.zemljevid("https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_SVN_shp.zip", "gadm36_SVN_1")
-zemljevid$NAME_1[zemljevid$NAME_1 == "GoriĹˇka"] <- "Goriška"
-zemljevid$NAME_1[zemljevid$NAME_1 == "KoroĹˇka"] <- "Koroška"
-zemljevid$NAME_1[zemljevid$NAME_1 == "Notranjsko-kraĹˇka"] <- "Notranjsko-kraška"
-zemljevid$NAME_1[zemljevid$NAME_1 == "Obalno-kraĹˇka"] <- "Obalno-kraška"
-zemljevid1 <- tm_shape(merge(zemljevid, povprecje.diplomantov.po.regijah, by.x="NAME_1", by.y="regija" )) + 
-  tm_polygons("povprecje",title="Povprečje",palette="Purples") + tm_style("grey") + tm_layout(title="Povprečno število diplomantov po regijah v zadnjih desetih letih")
-
-# GRAF POVPRECJA DIJAKOV GLEDE NA VRSTO IZOBRAZEVANJA IN SPLOL
-
-podatki5 <- tabela2nova %>% group_by(izobrazevanje, spol) %>% summarise(povprecje=sum(stevilo)/10)
-graf5 <- ggplot(podatki5, aes(x=izobrazevanje, y=povprecje, fill = spol)) + geom_col(position = 'dodge')  + 
-  coord_flip() + labs(x = "Vrsta izobraževanja", y = "Povprečno število", title = "Povprečno število moških in žensk na leto v \nposameznih vrstah izobraževanj v \nzadnjih desetih letih") +
-  scale_fill_discrete(name = "Spol", labels = c("moški", "ženski")) + scale_fill_brewer()
-
-# GRAF POVPRECJA DIPLOMANTOV GLEDE NA VRSTO IZOBRAZEVANJA IN SPLOL
-
-podatki6 <- tabela3nova %>% group_by(izobrazevanje, spol) %>% summarise(povprecje=sum(stevilo)/10)
-graf6 <- ggplot(podatki6, aes(x=izobrazevanje, y=povprecje, fill = spol)) + geom_col(position = 'dodge')  + 
-  coord_flip() + labs(x = "Vrsta izobraževanja", y = "Povprečno število", title = "Povprečno število diplomantov na leto  \nposameznih vrstah izobraževanj v \nzadnjih desetih letih") +
-   scale_fill_discrete(name = "Spol", labels = c("moški", "ženski")) + scale_fill_brewer()
+graf_povprecja_po_regijah <- ggplot(povprecje.dijakovindiplomantov.po.regijah, aes(x=regija, y=povprecje, fill=kategorija)) +
+  geom_col(position = 'dodge')  + 
+  coord_flip() +
+  labs(x = "Regija", y = "Povprečno število", title = "Povprečno število dijakov in diplomantov na leto v \nposameznih statističnih regijah") +
+  scale_fill_discrete(name = "Kategorija", labels = c("število dijakov", "število diplomantov")) + 
+  scale_fill_brewer()
 
 # TORTNI DIAGRAM DIPLOMANTOV GLEDE NA NACIN STUDIJA s knjižnjico plotrix
 
@@ -67,17 +42,67 @@ zemljevida$NAME_1[zemljevida$NAME_1 == "KoroĹˇka"] <- "Koroška"
 zemljevida$NAME_1[zemljevida$NAME_1 == "Notranjsko-kraĹˇka"] <- "Notranjsko-kraška"
 zemljevida$NAME_1[zemljevida$NAME_1 == "Obalno-kraĹˇka"] <- "Obalno-kraška"
 zemljevid2 <- tm_shape(merge(zemljevida, povprecjedipl, by.x="NAME_1", by.y="regija" )) + 
-  tm_polygons("povprecje",title="Delež",palette="Purples")+ tm_style("grey") + tm_layout(main.title="Povprečni delež diplomantov po regijah v zadnjih desetih \nletih glede na število prebivalcev v posamezni regiji") + tm_text(text='NAME_1', size=0.6)
+  tm_polygons("povprecje",title="Delež",palette="Purples")+ tm_style("grey") + tm_layout(main.title="Povprečni delež diplomantov po regijah \nv zadnjih desetih letih glede na \nštevilo prebivalcev v posamezni regiji") + tm_text(text='NAME_1', size=0.6)
 
 povprecjedij <- skupnatabela %>% group_by(regija) %>% summarise(povprecje=sum(delez)/10)
 zemljevida2 <- uvozi.zemljevid("https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_SVN_shp.zip", "gadm36_SVN_1")
+
 zemljevida2$NAME_1[zemljevida2$NAME_1 == "GoriĹˇka"] <- "Goriška"
 zemljevida2$NAME_1[zemljevida2$NAME_1 == "KoroĹˇka"] <- "Koroška"
 zemljevida2$NAME_1[zemljevida2$NAME_1 == "Notranjsko-kraĹˇka"] <- "Notranjsko-kraška"
 zemljevida2$NAME_1[zemljevida2$NAME_1 == "Obalno-kraĹˇka"] <- "Obalno-kraška"
-zemljevid3 <- tm_shape(merge(zemljevida2, povprecjedij, by.x="NAME_1", by.y="regija" )) + 
-  tm_polygons("povprecje",title="Delež",palette="Blues")+ tm_style("grey") + tm_layout(main.title="Povprečni delež dijakov po regijah v zadnjih desetih \nletih glede na število prebivalcev v posamezni regiji")+ tm_text(text='NAME_1', size=0.6)
 
+zemljevid3 <- tm_shape(merge(zemljevida2, povprecjedij, by.x="NAME_1", by.y="regija" )) + 
+  tm_polygons("povprecje",title="Delež",palette="Blues")+
+  tm_style("grey") +
+  tm_layout(main.title="Povprečni delež dijakov po regijah \nv zadnjih desetih letih glede na \nštevilo prebivalcev v posamezni regiji")+ tm_text(text='NAME_1', size=0.6)
+
+
+# ZEMLJEVID POVPRECJA DIPLOMANTOV PO REGIJAH, KI GA NA KONCU NISEM UPORABILA, KER JE ZEMLJEVID2 BOLJŠI NAČIN PRIKAZOVANJA
+
+zemljevid <- uvozi.zemljevid("https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_SVN_shp.zip", "gadm36_SVN_1")
+
+zemljevid$NAME_1[zemljevid$NAME_1 == "GoriĹˇka"] <- "Goriška"
+zemljevid$NAME_1[zemljevid$NAME_1 == "KoroĹˇka"] <- "Koroška"
+zemljevid$NAME_1[zemljevid$NAME_1 == "Notranjsko-kraĹˇka"] <- "Notranjsko-kraška"
+zemljevid$NAME_1[zemljevid$NAME_1 == "Obalno-kraĹˇka"] <- "Obalno-kraška"
+
+zemljevid1 <- tm_shape(merge(zemljevid, povprecje.diplomantov.po.regijah, by.x="NAME_1", by.y="regija" )) + 
+  tm_polygons("povprecje",title="Povprečje",palette="Purples") +
+  tm_style("grey") +
+  tm_layout(title="Povprečno število diplomantov po regijah v zadnjih desetih letih")
+
+# GRAF POVPRECJA DIJAKOV GLEDE NA VRSTO IZOBRAZEVANJA IN SPLOL (TO SEM KASNEJE PRIKAZALA V SHINYJU)
+
+podatki5 <- tabela2nova %>% group_by(izobrazevanje, spol) %>% summarise(povprecje=sum(stevilo)/10)
+graf5 <- ggplot(podatki5, aes(x=izobrazevanje, y=povprecje, fill = spol)) +
+  geom_col(position = 'dodge')  + 
+  coord_flip() + 
+  labs(x = "Vrsta izobraževanja", y = "Povprečno število", title = "Povprečno število moških in žensk na leto v \nposameznih vrstah izobraževanj v \nzadnjih desetih letih") +
+  scale_fill_discrete(name = "Spol", labels = c("moški", "ženski")) + 
+  scale_fill_brewer()
+
+# GRAF POVPRECJA DIPLOMANTOV GLEDE NA VRSTO IZOBRAZEVANJA IN SPLOL (TO SEM KASNEJE PRIKAZALA V SHINYJU)
+
+podatki6 <- tabela3nova %>% group_by(izobrazevanje, spol) %>% summarise(povprecje=sum(stevilo)/10)
+graf6 <- ggplot(podatki6, aes(x=izobrazevanje, y=povprecje, fill = spol)) +
+  geom_col(position = 'dodge')  + 
+  coord_flip() +
+  labs(x = "Vrsta izobraževanja", y = "Povprečno število", title = "Povprečno število diplomantov na leto  \nposameznih vrstah izobraževanj v \nzadnjih desetih letih") +
+  scale_fill_discrete(name = "Spol", labels = c("moški", "ženski")) +
+  scale_fill_brewer()
+
+
+
+
+
+
+
+
+
+
+
+# POISKUSI
 #graf2 <- ggplot(povprecje.diplomantov.po.regijah, aes(regija, povprecje, group = 1)) + geom_col() + coord_flip() +
 #  labs(x = "Regija", y = "Povprecje", 
 #       title = "Povprečno število diplomantov po regijah v zadnjih desetih letih")
